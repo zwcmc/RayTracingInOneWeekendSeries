cd444ee380f6849254c3681e463487a1ac32e1a3
chenzwsss <746869297@qq.com>
2024-09-22 21:10:50 +0800

    Ray Tracing The Next Week

---
+++ RayTracingTheNextWeek/build/CMakeFiles/RayTracingTheNextWeek.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracingTheNextWeek.dir/main.cpp.o"
+  "CMakeFiles/RayTracingTheNextWeek.dir/main.cpp.o.d"
+  "RayTracingTheNextWeek"
+  "RayTracingTheNextWeek.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracingTheNextWeek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RayTracingTheNextWeek/build/CMakeFiles/RayTracingTheNextWeek.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RayTracingTheNextWeek.
+# This may be replaced when dependencies are built.
