a8beef3c3d24cf614a4ba1b51dfd47450adb2de8
chenzwsss <746869297@qq.com>
2024-09-25 00:34:18 +0800

    Update

--- RayTracingTheRestOfYourLife/main.cpp
+++ RayTracingTheRestOfYourLife/main.cpp
@@ -1,77 +1,244 @@
-// #include "rtweekend.h"
+// #include <random>
+// #include <time.h>
+// #include <iostream>
+// #include <iomanip>
 
-// #include "camera.h"
-// #include "hittable_list.h"
-// #include "material.h"
-// #include "quad.h"
-// #include "sphere.h"
+// #include <chrono>
+// #include <thread>
+
+// double f(float x)
+// {
+//     return 3.0 * x * x;
+// }
 
 // int main()
 // {
-//     hittable_list world;
+//     std::cout << std::fixed << std::setprecision(12);
 
-//     auto red = make_shared<lambertian>(color(.65, .05, .05));
-//     auto white = make_shared<lambertian>(color(.73, .73, .73));
-//     auto green = make_shared<lambertian>(color(.12, .45, .15));
-//     auto light = make_shared<diffuse_light>(color(15, 15, 15));
+//     int n = 0;
+//     float a = 1.0f, b = 3.0f;
+//     double sum = 0.0;
+//     std::default_random_engine seed(time(NULL));
+//     std::uniform_real_distribution<float> random(a, b);
 
-//     // Cornell box sides
-//     world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 0, 555), vec3(0, 555, 0), green));
-//     world.add(make_shared<quad>(point3(0, 0, 555), vec3(0, 0, -555), vec3(0, 555, 0), red));
-//     world.add(make_shared<quad>(point3(0, 555, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
-//     world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 0, -555), white));
-//     world.add(make_shared<quad>(point3(555, 0, 555), vec3(-555, 0, 0), vec3(0, 555, 0), white));
+//     std::chrono::milliseconds interval(1);
 
-//     // Light
-//     world.add(make_shared<quad>(point3(213, 554, 227), vec3(130, 0, 0), vec3(0, 0, 105), light));
+//     while (true)
+//     {
+//         auto start = std::chrono::steady_clock::now();
 
-//     // Box 1
-//     shared_ptr<hittable> box1 = box(point3(0, 0, 0), point3(165, 330, 165), white);
-//     box1 = make_shared<rotate_y>(box1, 15);
-//     box1 = make_shared<translate>(box1, vec3(265, 0, 295));
-//     world.add(box1);
+//         n++;
+//         float sample = random(seed);
+//         sum += f(sample);
 
-//     // Box 2
-//     shared_ptr<hittable> box2 = box(point3(0, 0, 0), point3(165, 165, 165), white);
-//     box2 = make_shared<rotate_y>(box2, -18);
-//     box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-//     world.add(box2);
+//         if (n % 1000 == 0)
+//         {
+//             std::cout << "n = " << n  << ", and F_n(X) = " << (sum * (b - a) / n) << '\n';
+//         }
 
-//     camera cam;
+//         auto end = std::chrono::steady_clock::now();
+//         std::chrono::duration<double> elapsed = end - start;
 
-//     cam.aspect_ratio = 1.0;
-//     cam.image_width = 600;
-//     cam.samples_per_pixel = 64;
-//     cam.max_depth = 50;
-//     cam.background = color(0, 0, 0);
+//         std::chrono::milliseconds sleep_time = interval - std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
 
-//     cam.vfov = 40;
-//     cam.lookfrom = point3(278, 278, -800);
-//     cam.lookat = point3(278, 278, 0);
-//     cam.vup = vec3(0, 1, 0);
+//         if (sleep_time.count() > 0)
+//         {
+//             std::this_thread::sleep_for(sleep_time);
+//         }
+//     }
 
-//     cam.defocus_angle = 0;
-
-//     cam.render(world);
+//     return 0;
 // }
-#include "rtweekend.h"
+// #include <random>
+// #include <time.h>
+// #include <cmath>
+// #include <iostream>
 
+// #define PI 3.14159265358979323846
+
+// double random_generator1(double x)
+// {
+//     return x * PI / 2.0;
+// }
+
+// double random_generator2(double x)
+// {
+//     return sqrt(x) * PI / 2.0;
+// }
+
+// double pdf1(double x)
+// {
+//     return 2.0 / PI;
+// }
+
+// double pdf2(double x)
+// {
+//     return 8.0 * x / (PI * PI);
+// }
+
+// double f(double x)
+// {
+//     return sin(x);
+// }
+
+// double van_der_corput(int x, int base)
+// {
+//     double q = 0, bk = 1.0 / base;
+//     while (x > 0)
+//     {
+//         q += (x % base) * bk;
+//         x /= base;
+//         bk /= base;
+//     }
+//     return q;
+// }
+
+// int main()
+// {
+    // const int total_times = 16;
+    // int run_times = 1;
+    // double real = 1.0;
+    // std::default_random_engine seed(time(NULL));
+    // printf("No     Uniform     Importance   Err.Uniform(%%) Err.Importance(%%)\n");
+    // while ((run_times++) < total_times)
+    // {
+    //     int num_samples = 16;
+    //     double sum1 = 0.0, sum2 = 0.0;
+    //     std::uniform_real_distribution<double> random(0, 1);
+
+    //     for (int i = 0; i < num_samples; i++)
+    //     {
+    //         double sample = random(seed);
+    //         double random1 = random_generator1(sample);
+    //         double random2 = random_generator2(sample);
+    //         double p1 = pdf1(random1);
+    //         double p2 = pdf2(random2);
+
+    //         sum1 += f(random1) / p1;
+    //         sum2 += f(random2) / p2;
+    //     }
+    //     sum1 /= num_samples;
+    //     sum2 /= num_samples;
+    //     printf("%-6d %-12.4f %-14.4f %-14.1f %-12.1f\n", run_times, sum1, sum2, 100.0 * (sum1 - real) / real, 100.0 * (sum2 - real) / real);
+    // }
+//     for (int i = 1; i < 14; i++)
+//     {
+//         double s = van_der_corput(i, 10);
+//         std::cout << s << std::endl;
+//     }
+    
+//     return 0;
+// }
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
 #include <iostream>
-#include <iomanip>
+#include <fstream>
+#include <random>
+
+#define Float float
+
+const int Primes[] = {
+    2, 3, 5, 7, 11,
+    13, 17, 19, 23,
+    29, 31, 37, 41,
+    43, 47, 53, 59,
+    61, 67, 71, 73};
+
+const int Width = 512;
+const int Height = 512;
+const int NumSamples = 200;
+const int Strand = 3;
+
+const int Offset[9][2] = {
+    {0, 0},
+    {0, 1},
+    {0, -1},
+    {1, 0},
+    {-1, 0},
+    {1, 1},
+    {-1, -1},
+    {1, -1},
+    {-1, 1},
+};
+
+static Float RadicalInverse(int a, int base)
+{
+    const Float invBase = (Float)1 / (Float)base;
+    int reversedDigits = 0;
+    Float invBaseN = 1;
+    while (a)
+    {
+        int next = a / base;
+        int digit = a - next * base;
+        reversedDigits = reversedDigits * base + digit;
+        invBaseN *= invBase;
+        a = next;
+    }
+    return reversedDigits * invBaseN;
+}
+
+int GetNthPrime(int dimension)
+{
+    return Primes[dimension];
+}
+
+Float Halton(int dimension, int index)
+{
+    return RadicalInverse(index, GetNthPrime(dimension));
+}
+
+Float Hammersley(int dimension, int index, int numSamples)
+{
+    if (dimension == 0)
+        return index / (Float)numSamples;
+    else
+        return RadicalInverse(index, GetNthPrime(dimension - 1));
+}
+
+void WritePPM(const char *filename, const unsigned char *data, int width, int height)
+{
+    std::ofstream ofs;
+    ofs.open(filename);
+    ofs << "P5\n"
+        << width << " " << height << "\n255\n";
+    ofs.write((char *)data, width * height);
+    ofs.close();
+}
+
+void SaveSample(unsigned char *pixels, int width, int height, Float x, Float y)
+{
+    int px = (int)(x * Width + 0.5);
+    int py = (int)(y * Height + 0.5);
+    for (int i = 0; i < 9; i++)
+    {
+        int col = std::max(std::min(px + Offset[i][0], Width - 1), 0);
+        int row = std::max(std::min(py + Offset[i][1], Height - 1), 0);
+        pixels[row * width + col] = 0;
+    }
+}
 
 int main()
 {
-    int a = 0;
-    int b = 2;
-    int N = 1000000;
-    auto sum = 0.0;
-
-    for (int i = 0; i < N; i++)
+    unsigned char pixels[Height][Width];
+    // generate halton sequence.
+    memset(pixels, 0xff, sizeof(pixels));
+    for (int i = 0; i < NumSamples; i++)
     {
-        auto x = random_double(a, b);
-        sum += x * x;
+        Float x = Halton(0, i);
+        Float y = Halton(1, i);
+        SaveSample(&pixels[0][0], Width, Height, x, y);
     }
+    WritePPM("halton.ppm", &pixels[0][0], Width, Height);
 
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << (b - a) * (sum / N) << '\n';
+    // generate hammersley sequence.
+    memset(pixels, 0xff, sizeof(pixels));
+    for (int i = 0; i < NumSamples; i++)
+    {
+        Float x = Hammersley(0, i, NumSamples);
+        Float y = Hammersley(1, i, NumSamples);
+        SaveSample(&pixels[0][0], Width, Height, x, y);
+    }
+    WritePPM("hammersley.ppm", &pixels[0][0], Width, Height);
+    return 0;
 }
